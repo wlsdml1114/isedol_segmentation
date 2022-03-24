0b9c26dc34b1371a3e29d6136a4c7b111fb6c89c
wlsdml1114 <wlsdml1114@gmail.com>
2022-03-24 17:12:29 +0900

    augmentation

--- augmentation.py
+++ augmentation.py
@@ -6,26 +6,58 @@ from tqdm import tqdm
 import numpy as np
 from collections import Counter
 import argparse
+import torchvision.transforms.functional as TF
 import configparser
+from PIL import Image
 
 config = configparser.ConfigParser()    
 config.read('./setting.ini', encoding='CP949') 
 
-file_names = config['path']['name'].split('/')
-folder = file_names[-1]
+file_names = os.listdir(config['path']['ori_dir'])
 tr_data_dir = config['path']['tr_data_dir']
 aug_dir = config['path']['aug_dir']
 
-files = os.listdir(os.path.join(tr_data_dir,folder,"Masks"))
-if not(os.path.exists(os.path.join(aug_dir,folder,"Images"))):
-    os.system('mkdir -p '+os.path.join(aug_dir,folder,"Images"))
+for folder in file_names:
+    print()
+    '''file_name = file_name.split('/')
+    folder = file_name[-1]'''   
+    print(folder,'data augmentation start')
 
-if not(os.path.exists(os.path.join(aug_dir,folder,"Masks"))):
-    os.system('mkdir -p '+os.path.join(aug_dir,folder,"Masks"))
+    files = os.listdir(os.path.join(tr_data_dir,folder,"Masks"))
+    if not(os.path.exists(os.path.join(aug_dir,folder,"Images"))):
+        os.system('mkdir -p '+os.path.join(aug_dir,folder,"Images"))
 
-print('mask_counting')
-pix = []
-for idx in tqdm(range(len(files))):
-    img = cv2.imread(os.path.join(tr_data_dir,folder,"Images",files[idx][:-3]+'jpg'))
-    mask = cv2.imread(os.path.join(tr_data_dir,folder,"Masks",files[idx]))
+    if not(os.path.exists(os.path.join(aug_dir,folder,"Masks"))):
+        os.system('mkdir -p '+os.path.join(aug_dir,folder,"Masks"))
+
+    for idx in tqdm(range(len(files))):
+        img = cv2.imread(os.path.join(tr_data_dir,folder,"Images",files[idx][:-3]+'jpg'))      
+        try :  
+            pil_image = Image.fromarray(img)
+        except :
+            print(folder,files[idx],"Exception occur")
+            continue
+        flip_img = TF.hflip(pil_image)
+        bright_img=TF.adjust_brightness(pil_image,0.7)
+        cont_img=TF.adjust_contrast(pil_image,0.8)
+        crop_img = img[100:,100:]
+        cv2.imwrite(os.path.join(aug_dir,folder,"Images",'ori_%sjpg'%(files[idx][:-3])),img)
+        cv2.imwrite(os.path.join(aug_dir,folder,"Images",'flip_%sjpg'%(files[idx][:-3])),np.array(flip_img))
+        cv2.imwrite(os.path.join(aug_dir,folder,"Images",'bri_%sjpg'%(files[idx][:-3])),np.array(bright_img))
+        cv2.imwrite(os.path.join(aug_dir,folder,"Images",'cont_%sjpg'%(files[idx][:-3])),np.array(cont_img))
+        cv2.imwrite(os.path.join(aug_dir,folder,"Images",'crop_%sjpg'%(files[idx][:-3])),crop_img)
+
+
+
+        img = cv2.imread(os.path.join(tr_data_dir,folder,"Masks",files[idx]))
+        pil_image = Image.fromarray(img)
+        flip_img = TF.hflip(pil_image)
+        bright_img=TF.adjust_brightness(pil_image,0.7)
+        cont_img=TF.adjust_contrast(pil_image,0.8)
+        crop_img = img[100:,100:]
+        cv2.imwrite(os.path.join(aug_dir,folder,"Masks",'ori_%spng'%(files[idx][:-3])),img)
+        cv2.imwrite(os.path.join(aug_dir,folder,"Masks",'flip_%spng'%(files[idx][:-3])),np.array(flip_img))
+        cv2.imwrite(os.path.join(aug_dir,folder,"Masks",'bri_%spng'%(files[idx][:-3])),np.array(bright_img))
+        cv2.imwrite(os.path.join(aug_dir,folder,"Masks",'cont_%spng'%(files[idx][:-3])),np.array(cont_img))
+        cv2.imwrite(os.path.join(aug_dir,folder,"Masks",'crop_%spng'%(files[idx][:-3])),crop_img)
 
