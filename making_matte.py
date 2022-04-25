b2fc7a79fe8105acfe9184907faa86e2c2b7a8d6
wlsdml1114 <wlsdml1114@gmail.com>
2022-04-25 14:37:36 +0900

    merge test and modify requirement.txt

---
+++ making_matte.py
@@ -0,0 +1,70 @@
+import os
+import cv2
+from tqdm import tqdm
+import argparse
+from demo.image_matting.colab.inference2 import run
+import datetime
+import requests
+import subprocess
+print(datetime.datetime.now())
+
+
+def get_args():
+	
+	parser = argparse.ArgumentParser(description='Arguments for the testing purpose.')	
+	parser.add_argument('--ori_dir', type=str, required=True)
+	parser.add_argument('--file_name', type=str, required=True)
+	parser.add_argument('--jpg_dir', type=str,required=True)
+	parser.add_argument('--png_dir', type=str, required=True)
+	parser.add_argument('--token', type=str, required=True)
+	args = parser.parse_args()
+	return args
+
+
+args = get_args()
+
+slack_token = args.token
+file_name = args.file_name
+file_names = file_name.split('/')
+origin_file_name = file_names[-1]
+
+ori_dir = args.ori_dir
+jpg_dir = os.path.join(args.jpg_dir,origin_file_name)
+png_dir = os.path.join(args.png_dir,origin_file_name)
+
+
+print(origin_file_name,'start')
+if not(os.path.exists(jpg_dir)) : 
+    os.system('mkdir -p ' + jpg_dir)
+if not(os.path.exists(png_dir)) : 
+    os.system('mkdir -p ' + png_dir)
+
+cap = cv2.VideoCapture(os.path.join(ori_dir,origin_file_name))
+
+count = 0
+while True :
+
+    if (count%1000 == 0):
+        print(count)
+    ret, frame = cap.read()
+
+    if ret == False :
+        break
+
+    if origin_file_name[0] == 'l' :
+        cv2.imwrite(os.path.join(jpg_dir,'%d.jpg'%(count)),frame[400:,:800,:])
+    if origin_file_name[0] == 'r' :
+        cv2.imwrite(os.path.join(jpg_dir,'%d.jpg'%(count)),frame[400:,1200:,:])
+    count+=1
+
+<<<<<<< Updated upstream
+
+=======
+>>>>>>> Stashed changes
+token = slack_token
+channel = "#finish-alarm"
+text = origin_file_name+" making matte finish"
+
+requests.post("https://slack.com/api/chat.postMessage",
+    headers={"Authorization": "Bearer "+token},
+    data={"channel": channel,"text": text})
---
+++ matte.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+
+ori_dir='/data/codes/data/input'
+jpg_dir='/data/codes/data/output'
+seg_dir='/data/codes/data/segmentation'
+png_dir='/data/codes/data/temp'
+tr_data_dir='/data/codes/data/dataset'
+out_dir='/data/codes/data/mp4'
+model_dir='/data/codes/data/model'
+fps=60
+
+file=$1
+echo "$file" task start
+
+/home/smartai/anaconda3/envs/seg/bin/python /data/codes/isedol_segmentation/making_matte.py\
+    --ori_dir=$ori_dir \
+    --file_name=$file \
+    --png_dir=$png_dir \
+    --jpg_dir=$jpg_dir \
+<<<<<<< Updated upstream
+    --token=$SLACK_TOKEN
+    
+/home/smartai/anaconda3/envs/seg/bin/python /data/codes/isedol_segmentation/inference.py\
+=======
+    --token=$SLACK_TOKEN \
+    --wav_dir=$wav_dir
+
+/usr/anaconda3/envs/hair_task/bin/python /home/jini1114/git/isedol_segmentation/inference.py\
+>>>>>>> Stashed changes
+    --seg_dir=$seg_dir \
+    --file_name=$file \
+    --jpg_dir=$jpg_dir \
+    --model_dir=$model_dir \
+    --token=$SLACK_TOKEN
+
+/home/smartai/anaconda3/envs/seg/bin/python /data/codes/isedol_segmentation/png2mp4.py\
+    --file_name=$file \
+    --jpg_dir=$jpg_dir \
+    --png_dir=$png_dir \
+    --out_dir=$out_dir \
+    --fps=$fps \
+    --seg_dir=$seg_dir \
+    --token=$SLACK_TOKEN
+    
